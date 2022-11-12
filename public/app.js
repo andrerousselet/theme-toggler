eeba66edacfdb3770b218e35984fe842d3bd494b
Andre Rousselet <andre098@hotmail.com>
2022-11-12 13:55:19 -0300

    feat(buttons): apply logic to toggle between light and dark themes

--- public/app.js
+++ public/app.js
@@ -4,3 +4,11 @@ const darkButton = document.getElementById('dark');
 const lightButton = document.getElementById('light');
 const solarButton = document.getElementById('solar');
 const body = document.body;
+
+darkButton.onclick = () => {
+  body.classList.replace('light', 'dark');
+};
+
+lightButton.onclick = () => {
+  body.classList.replace('dark', 'light');
+};
